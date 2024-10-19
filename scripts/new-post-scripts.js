71cb249d8160d49c62190a6913bc2ebf0d22bb92
UnizaMatBre <breznican1@stud.uniza.sk>
2024-10-19 22:54:01 +0200

    feat: Added functionality to the buttons

--- scripts/new-post-scripts.js
+++ scripts/new-post-scripts.js
@@ -1,13 +1,47 @@
-const sanitizedEvalCode = function(textareaObj, canvasObj) {
-	alert(textareaObj.value);
+const sanitizeBindCode = function(canvasCtx, codeText) {
+	const locals = {};
 	
 	
+	const callableObj = Function("tick",
+		"(" + codeText + ")(this.canvasCtx, this.locals, tick);"
+	).bind({
+		"canvasCtx": canvasCtx,
+		"locals": locals
+	});
+	
+	return callableObj;
 }
 
 const registerAllEventHandlers = function(event) {
 	document.getElementById("post-code-eval-input").addEventListener("click", (eventObj) => {
-		alert("eval click");
+		const canvasObj = document.getElementById("post-code-canvas");
+		const canvasCtx = canvasObj.getContext("2d");
+		const textinObj = document.getElementById("post-code-input-area");
+		
+		// clear canvas
+		canvasCtx.clearRect(0, 0, canvasObj.width, canvasObj.height);
+		
+		// create sanitized funtion
+		const sanitizedCallable = sanitizeBindCode(canvasCtx, textinObj.value);
+	
+		
+		var tick = 0;
+		
+		const intervalId = setInterval(() => {
+			if(tick >= 360) {
+				alert("done");
+				clearInterval(intervalId);
+			};
+			
+			sanitizedCallable(tick);
+			
+			tick++;
+		}, 10);
 		
 	});
 	
-}
+	document.getElementById("post-submit-button").addEventListener("click", (eventObj) => {
+		alert("This would validate inputs and use 'fetch' to send them to the server - if there was any server in the first place");
+		
+	});
+};
